457671be22e7e432ab17a1619d60250172182ea3
sukatihte-cloud <suka.ti.hte@gmail.com>
2026-05-13 22:43:01 +0300

    Update server.py

--- server.py
+++ server.py
@@ -28,5 +28,6 @@ def check_db():
         return jsonify({"status": "Error", "message": f"Мелстрой всё еще против: {str(e)}"})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # Если Render не дал порт, используем 5000 по умолчанию
+    server_port = int(os.environ.get("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=server_port)
