a556838b870e1b6f0b58a7c8fff9affdf990d516
sukatihte-cloud <suka.ti.hte@gmail.com>
2026-05-13 22:35:20 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,43 +1,17 @@
 import os
 import psycopg2
-from psycopg2.extras import RealDictCursor
-from flask import Flask, render_template_string, request, jsonify
-from flask_cors import CORS
-from waitress import serve
 
-app = Flask(__name__)
-CORS(app)
-
-DB_URL = os.environ.get("DATABASE_URL")
+# Берем данные из Environment
+DB_PASS = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
 
 def get_db():
-    return psycopg2.connect(DB_URL, cursor_factory=RealDictCursor)
+    return psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password=DB_PASS,
+        host=DB_HOST,
+        port="5432"
+    )
 
-@app.route('/')
-def index():
-    return "<h1>Чат Ханифа живет тут!</h1><p>Если видишь это, значит сервер работает.</p>"
-
-@app.route('/send', methods=['POST'])
-def send():
-    data = request.json
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("INSERT INTO messages (username, content) VALUES (%s, %s)", (data['user'], data['text']))
-    conn.commit()
-    cur.close()
-    conn.close()
-    return jsonify({"status": "ok"})
-
-@app.route('/get')
-def get():
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("SELECT username as user, content as text FROM messages ORDER BY created_at DESC LIMIT 50")
-    messages = cur.fetchall()
-    cur.close()
-    conn.close()
-    return jsonify(messages)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    serve(app, host='0.0.0.0', port=port)
+# ... остальной код (index, send, get) такой же
